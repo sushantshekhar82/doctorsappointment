885878a60b0d4fcec6bf89c4ccd2fcba888b78bd
unknown <sushant.shekhar151997@gmail.com>
2023-08-01 23:44:49 +0530

    started new project doctors appointment

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className="text-[20px] text-blue-600">hello</h1>
+      
+    </>
+  )
+}
+
+export default App
